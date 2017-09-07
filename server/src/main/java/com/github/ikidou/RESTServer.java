dae212e6f7beb942088288c9930b1d7a5b829bc5
baiqiantao <baiqiantao@sina.com>
2017-09-07 21:23:26 +0800

    2017-9-7 21:23:12 星期四

--- server/src/main/java/com/github/ikidou/RESTServer.java
+++ server/src/main/java/com/github/ikidou/RESTServer.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2016 ikidou
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.github.ikidou;
 
 import com.github.ikidou.db.DB;
@@ -25,59 +9,49 @@ import com.google.gson.Gson;
 
 import java.sql.SQLException;
 
-import spark.ExceptionHandler;
-import spark.Filter;
-import spark.Request;
-import spark.Response;
 import spark.Spark;
 
 public class RESTServer {
-    static final String TYPE = "application/json; charset=UTF-8";
-
-    public static void main(String[] args) throws SQLException {
-        DB.init();
-
-        Spark.init();
-
-        Spark.get("/blog", BlogHandler.GET, GsonTransformer.getDefault());
-        Spark.get("/blog/:id", BlogHandler.GET, GsonTransformer.getDefault());
-
-        Spark.post("/blog", BlogHandler.POST, GsonTransformer.getDefault());
-
-        Spark.put("/blog", BlogHandler.PUT, GsonTransformer.getDefault());
-        Spark.put("/blog/:id", BlogHandler.PUT, GsonTransformer.getDefault());
-
-        Spark.delete("/blog", BlogHandler.DELETE, GsonTransformer.getDefault());
-        Spark.delete("/blog/:id", BlogHandler.DELETE, GsonTransformer.getDefault());
-
-        Spark.head("/blog", BlogHandler.HEAD, GsonTransformer.getDefault());
-        Spark.head("/blog/:id", BlogHandler.HEAD, GsonTransformer.getDefault());
-
-        Spark.post("/form", new FormHandler(), GsonTransformer.getDefault());
-
-        Spark.get("/headers", new HeaderHandler(), GsonTransformer.getDefault());
-
-        Spark.after(new Filter() {
-            @Override
-            public void handle(Request request, Response response) throws Exception {
-                response.type(TYPE);
-                response.header("author", "ikidou");
-            }
-        });
-
-        Spark.exception(RuntimeException.class, new ExceptionHandler() {
-            @Override
-            public void handle(Exception exception, Request request, Response response) {
-                Gson gson = new Gson();
-                response.body(gson.toJson(Resp.create(400, exception.getMessage())));
-            }
-        });
-        Spark.exception(Exception.class, new ExceptionHandler() {
-            @Override
-            public void handle(Exception exception, Request request, Response response) {
-                Gson gson = new Gson();
-                response.body(gson.toJson(Resp.create(500, exception.getMessage())));
-            }
-        });
-    }
+	private static final String TYPE = "application/json; charset=UTF-8";
+	
+	/**
+	 * 启动服务器
+	 */
+	public static void main(String[] args) throws SQLException {
+		DB.init();
+		
+		Spark.init();
+		
+		Spark.get("/blog", BlogHandler.GET, GsonTransformer.getDefault());
+		Spark.get("/blog/:id", BlogHandler.GET, GsonTransformer.getDefault());
+		
+		Spark.post("/blog", BlogHandler.POST, GsonTransformer.getDefault());
+		
+		Spark.put("/blog", BlogHandler.PUT, GsonTransformer.getDefault());
+		Spark.put("/blog/:id", BlogHandler.PUT, GsonTransformer.getDefault());
+		
+		Spark.delete("/blog", BlogHandler.DELETE, GsonTransformer.getDefault());
+		Spark.delete("/blog/:id", BlogHandler.DELETE, GsonTransformer.getDefault());
+		
+		Spark.head("/blog", BlogHandler.HEAD, GsonTransformer.getDefault());
+		Spark.head("/blog/:id", BlogHandler.HEAD, GsonTransformer.getDefault());
+		
+		Spark.post("/form", new FormHandler(), GsonTransformer.getDefault());
+		
+		Spark.get("/headers", new HeaderHandler(), GsonTransformer.getDefault());
+		
+		Spark.after((request, response) -> {
+			response.type(TYPE);
+			response.header("author", "ikidou");
+		});
+		
+		Spark.exception(RuntimeException.class, (exception, request, response) -> {
+			Gson gson = new Gson();
+			response.body(gson.toJson(Resp.create(400, exception.getMessage())));
+		});
+		Spark.exception(Exception.class, (exception, request, response) -> {
+			Gson gson = new Gson();
+			response.body(gson.toJson(Resp.create(500, exception.getMessage())));
+		});
+	}
 }
